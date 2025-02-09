3ad5b3879448a017eb1caf44a19bb99e5fe63990
Ashish <dhakalashish18@gmail.com>
2025-02-09 22:44:12 +0545

    fix: build issue

--- src/utils/removeBlankAttrs.ts
+++ src/utils/removeBlankAttrs.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 const removeBlankAttributes = (obj: any) =>
   Object.entries(obj).reduce((a: any, [k, v]) => {
