bf5e3b355f461adbb6d0c1eea174488f6541416a
Ashish <dhakalashish18@gmail.com>
2025-02-15 22:16:22 +0545

    feat: page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import { Footer, Header, Main } from "@/components";
+"use client";
+import {Header, Main } from "@/components";
 
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <>
+      <div className="flex h-screen flex-col" style={{ 
+        backgroundImage: `url('/assets/bg.jpg')`,
+         backgroundSize: 'cover',
+          backgroundPosition: 'center'
+          }}>
+       <div className="absolute inset-0 backdrop-blur-sm bg-black/50"></div>
+       <Header />
+       <Main />
+
+        </div>
+      
+
+    </>
   );
 }
