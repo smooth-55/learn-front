5ea5c188dd8d4d31c9a0c546704fc53099ba33a9
Ashish <dhakalashish18@gmail.com>
2025-02-15 22:16:29 +0545

    feat: header

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -1,16 +1,26 @@
 import { Col, Row } from "antd";
+import Link from "next/link";
 import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <Row className={"bg-[#566884]"}>
-      <Col>Home</Col>
-      <Col>About</Col>
-      <Col>Projects</Col>
-      <Col>Skills</Col>
-      <Col>Blog</Col>
-      <Col>Contact</Col>
-    </Row>
+    <Col className={"flex items-center justify-between mx-10 text-white"}>
+      <Row className="hover:cursor-pointer">
+        <Link href="/">
+        Logo
+        </Link>
+      </Row>
+      <Row className="gap-5">
+      <Col className="hover:text-[red] hover:font-bold hover:cursor-pointer">Home</Col>
+      <Col className="hover:text-[red] hover:font-bold hover:cursor-pointer">About</Col>
+      <Col className="hover:text-[red] hover:font-bold hover:cursor-pointer">Projects</Col>
+      <Col className="hover:text-[red] hover:font-bold hover:cursor-pointer">Skills</Col>
+      <Col className="hover:text-[red] hover:font-bold hover:cursor-pointer">Blog</Col>
+      <Col className="hover:text-[red] hover:font-bold hover:cursor-pointer">Contact</Col>
+
+      </Row>
+      
+    </Col>
   );
 };
 
