4ebfa45c07a225b3580a288c9066143710f972dd
Ashish <dhakalashish18@gmail.com>
2025-02-15 22:16:40 +0545

    feat: flip text

--- src/components/molecules/Main/index.tsx
+++ src/components/molecules/Main/index.tsx
@@ -1,8 +1,20 @@
-import { Col } from "antd";
-import React from "react";
+"use client";
+import { Col, Typography } from "antd";
+import React, { useEffect, useState } from "react";
 
 const Main: React.FC = () => {
-  return <Col>Main content goes here</Col>;
+  const words = ["Hello There", "It's Ashish Dhakal", "Passionate Full Stack Developer"];
+   const [index, setIndex] = useState(0);
+   useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+  return <Col className="h-[100vh] text-white flex flex-col items-center justify-center gap-0">
+      <Typography.Text className="!text-5xl font-bold text-white">{words[index]}</Typography.Text>
+      
+  </Col>;
 };
 
 export { Main };
