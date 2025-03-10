e941bb2270d34012bfbf1b482ee6a7f3f64d4414
Ashish <dhakalashish18@gmail.com>
2025-03-10 18:26:53 +0545

    fix: about section

--- src/components/molecules/About/index.tsx
+++ src/components/molecules/About/index.tsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+
 const About = () => {
   return (
-    <div className="calc_height bg-orange-300" id="about">
-      <p> ABOUT CONTAINER</p>
+    <div
+      id="about"
+      className="calc_height bg-black px-10 flex flex-col items-center justify-center w-full"
+    >
+      <div className="flex flex-col-reverse justify-center gap-[100px]  md:flex-row   items-center ">
+        <div className="left__section px-10 flex flex-col text-center items-center md:w-[50%] ">
+          <p className=" text-3xl">{"Ashish Dhakal"}</p>
+          <p className="text-xl">
+            {`I've spent over three years honing my skills as a fullstack software
+            developer primarily focusing on backend development with robust
+            problem-solving skills and a keen interest in learning new things in
+            web and technology.`}
+          </p>
+        </div>
+        <div className="right__section h-full  md:w-[50%] ">
+          <Image
+            className=""
+            src="/assets/1.jpg"
+            alt="profile"
+            width={800}
+            height={500}
+          />
+        </div>
+      </div>
     </div>
   );
 };
